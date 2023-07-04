6badc55d17a80c7b1bfbd06b2ae720dc99ef2498
Caio Rocha Sobreira <crocha-s@c1r6s4.42porto.com>
2023-07-04 18:58:18 +0100

    day 4

--- src/validate_map.c
+++ src/validate_map.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   validate_map.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: admin <admin@student.42.fr>                +#+  +:+       +#+        */
+/*   By: crocha-s <crocha-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/03 16:59:44 by crocha-s          #+#    #+#             */
-/*   Updated: 2023/07/03 21:31:36 by admin            ###   ########.fr       */
+/*   Updated: 2023/07/04 17:57:42 by crocha-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,4 +43,57 @@ int validate_walls(t_map *map)
 	return (1);
 }
 
-int validate_tiles()
+int validate_tiles(t_map *map, t_game *so_long)
+{
+	t_uint i;
+	t_uint j;
+
+	i = -1;
+	while (++i< map->rows)
+	{
+		j = -1;
+		while (++j < map->cols)
+		{
+			if (map->tiles[i][j] == PLAYER)
+			{
+				map->players++;
+				so_long->curr = (t_point){j, i};
+				so_long->next = so_long->curr;
+			}
+			else if (map->tiles[i][j] == EXIT)
+				map->exits++;
+			else if (map->tiles[i][j] == COIN)
+				map->coins++;
+			else if(!ft_strchr(TILES,map->tiles[i][j]))
+				return (0);
+		}
+	}
+	return (players == 1 && exits == 1 && coins => 1);
+}
+
+int validate_paths(t_map *map, t_game *so_long)
+{
+	t_uint i;
+	int found_exit;
+	char **tiles;
+
+	i = -1;
+	found_exit = 0;
+
+	tiles = ft_calloc (map->rows +1, sizeof(char *));
+	if(!tiles)
+		exit(1);
+	while(++i < map->rows)
+	{
+		tiles[i] = ft_strdup(map->tiles[i]);
+		{
+			if(!tiles[i])
+			{
+				clean_tiles(tiles);
+				exit(1);
+			}
+		}
+	}
+	
+	return (found_exit);
+}
